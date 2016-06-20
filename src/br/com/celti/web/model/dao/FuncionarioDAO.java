28a05c23acb34395cb1006553b1651a17d24263f
Suzan Nely <snelycardosooliveira5@gmail.com>
2016-06-19 21:22:58 -0300

    Método para efetuar consulta de login do funcionario criada.

--- src/br/com/celti/web/model/dao/FuncionarioDAO.java
+++ src/br/com/celti/web/model/dao/FuncionarioDAO.java
@@ -32,6 +32,18 @@ public class FuncionarioDAO {
 		return entityManager.getReference(Funcionario.class, id);
 	}
 
+	public Funcionario validar(String login, String senha) {
+		String jpql = "Select f from funcionario f where f.login = :customLogin";
+		Query query = entityManager.createQuery(jpql).setParameter("customLogin", login);
+		List<Funcionario> l = query.getResultList();
+		
+		if (l.size() == 0) {
+			return null;
+		} else {
+			return l.get(0);
+		}
+	}
+	
 	public List<Funcionario> listar() {
 		String jpql = "Select f from funcionario f order by nome";
 		Query query = entityManager.createQuery(jpql);
--- src/br/com/celti/web/model/dao/test/FuncionarioDAOTest.java
+++ src/br/com/celti/web/model/dao/test/FuncionarioDAOTest.java
@@ -10,7 +10,7 @@ import br.com.celti.web.model.dao.FuncionarioDAO;
 import br.com.celti.web.model.dao.JPAUtil;
 
 public class FuncionarioDAOTest {
-
+/*
 	@Test
 	public void testCadastrar() {
 
@@ -25,8 +25,8 @@ public class FuncionarioDAOTest {
 				
 		tf.setId(new Long(1));
 		
-		f.setNome("Matheus");
-		f.setLogin("matheus");
+		f.setNome("Fulano de Almeida");
+		f.setLogin("fulano");
 		f.setSenha("123");
 		f.setContato("contato");
 		f.setEndereco("endereco");
@@ -37,6 +37,24 @@ public class FuncionarioDAOTest {
 		entitymanager.getTransaction().commit();
 		entitymanager.close();
 		
+	}
+	*/
+	@Test 
+	public void testValidar(){
+		
+		EntityManager entitymanager = JPAUtil.getEntityManager();
+		
+		FuncionarioDAO fDAO = new FuncionarioDAO(entitymanager);
+
+		entitymanager.getTransaction().begin();
+		
+		Funcionario f = new Funcionario();
+			
+		f = fDAO.validar("fulano", "123");
+
+		entitymanager.getTransaction().commit();
+		
+		System.out.println(f.getNome());
 	}
 
 }
